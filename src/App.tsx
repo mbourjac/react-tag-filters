cabc1497d3ffc6aa632dac1f36bfc7171dbe3714
Michaël Bourjac <michael.bourjac@gmail.com>
2024-02-16 19:36:03 +0100

    feat: create base routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,18 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import { AppLayout } from './layouts/AppLayout';
+import { About } from './pages/About';
+import { Blog } from './pages/Blog';
 import { Home } from './pages/Home';
+import { Projects } from './pages/Projects';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<AppLayout />}>
       <Route index element={<Home />} />
+      <Route path="projects" element={<Projects />} />
+      <Route path="about" element={<About />} />
+      <Route path="blog" element={<Blog />} />
     </Route>,
   ),
 );
